0fbe28d162b3310deb54e38063a7f7a9cf813dc1
HuseynliTurane <13612@holbertonstudents.com>
2026-05-07 03:22:18 -0400

    task77888

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ps -u "$1" -F | grep -vE '(\s+0){2}\s+'
+
+# Check if a username was provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <username>"
+    exit 1
+fi
+
+# Monitor processes started by the specified user
+# Exclude processes where VSZ or RSS is 0
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command \
+| grep -vE '^[[:space:]]*USER' \
+| awk '$5 != 0 && $6 != 0'
