8fcbe3ef58f2a2420328cd9d6a58a984d446a390
HuseynliTurane <13612@holbertonstudents.com>
2026-06-12 09:10:48 -0400

    task5

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | while read -r sub; do echo "$sub"; ip=$(dig +short $sub | tail -n1); [ -n "$ip" ] && echo "$sub,$ip" >> $1.txt; done
+subfinder -d $1 -silent | tee /dev/stderr | xargs -I {} sh -c 'echo -n "{},"; dig +short {} | tail -n1' | grep -E ',[0-9]' > $1.txt
